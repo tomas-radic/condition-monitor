3d1d1ad1de0f43a8ecf4ce7993a79e15b7c5b061
Tomas Radic <tomas.radic@gmail.com>
2017-12-08 11:54:01 +0100

    API rejects new measurements unless the latest one is at least 10 minutes old

--- app/controllers/api/measurements_controller.rb
+++ app/controllers/api/measurements_controller.rb
@@ -3,6 +3,12 @@ class Api::MeasurementsController < ApplicationController
   before_action :authenticate
 
   def create
+    last_measurement = Measurement.all.order(measured_at: :desc).first
+
+    if last_measurement.present? && (last_measurement.measured_at > (Time.now - 10.minutes))
+      render json: { status: 208 } and return
+    end
+
     temperature = params[:temperature].to_f.round.to_i
     humidity = params[:humidity].to_f.round.to_i
     measurement = Measurement.create(
